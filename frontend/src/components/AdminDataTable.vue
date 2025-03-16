583d952e1507512544c896a0a04114cd8d576130
ElyesLaaribi <elyeslaaribii@gmail.com>
2025-03-16 15:00:36 +0100

    minor change in ui

--- frontend/src/components/AdminDataTable.vue
+++ frontend/src/components/AdminDataTable.vue
@@ -148,7 +148,7 @@ const submit = async () => {
                       id="first-name"
                       name="first-name"
                       autocomplete="given-name"
-                      class="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
+                      class="bg-white mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
                     />
                     <p v-if="errors.name" class="text-red-500 text-xs mt-1">
                       {{ errors.name[0] }}
@@ -166,7 +166,7 @@ const submit = async () => {
                       id="email"
                       name="email"
                       autocomplete="email"
-                      class="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
+                      class="bg-white mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
                     />
                     <p v-if="errors.email" class="text-red-500 text-xs mt-1">
                       {{ errors.email[0] }}
@@ -184,7 +184,7 @@ const submit = async () => {
                       id="password"
                       name="password"
                       autocomplete="new-password"
-                      class="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
+                      class="bg-white mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
                     />
                     <p v-if="errors.password" class="text-red-500 text-xs mt-1">
                       {{ errors.password[0] }}
@@ -202,7 +202,7 @@ const submit = async () => {
                       id="password-confirmation"
                       name="password-confirmation"
                       autocomplete="new-password"
-                      class="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
+                      class="bg-white mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
                     />
                   </div>
                 </div>
