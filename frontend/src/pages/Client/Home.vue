c6168ce34f33fb542f6e3607934b74f9bb99b5e7
ElyesLaaribi <elyeslaaribii@gmail.com>
2025-03-28 17:54:46 +0100

    UI client browse all

--- frontend/src/pages/Client/Home.vue
+++ frontend/src/pages/Client/Home.vue
@@ -1,48 +1,25 @@
 <script setup>
+import { onMounted, ref } from "vue";
 import DefaultLayout from "../../components/DefaultLayout.vue";
-const products = [
-  {
-    id: 1,
-    name: "Earthen Bottle",
-    href: "#",
-    price: "$48",
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-  },
-  {
-    id: 2,
-    name: "Nomad Tumbler",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-02.jpg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
-  },
-  {
-    id: 3,
-    name: "Focus Paper Refill",
-    href: "#",
-    price: "$89",
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-03.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    id: 4,
-    name: "Machined Mechanical Pencil",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-04.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  // More products...
-];
+import api from "../../axios";
+
+const products = ref([]);
+
+onMounted(() => {
+  api.get("/api/public-listings").then((response) => {
+    console.log(response.data);
+    products.value = response.data.data.map((product) => {
+      return {
+        ...product,
+        imageSrc:
+          product.images && product.images.length
+            ? product.images[0]
+            : "fallback-image.jpg",
+        imageAlt: product.name || "Product image",
+      };
+    });
+  });
+});
 </script>
 
 <template>
@@ -75,9 +52,11 @@ const products = [
               :alt="product.imageAlt"
               class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
             />
-            <h3 class="mt-4 text-sm text-gray-700">{{ product.name }}</h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">
-              {{ product.price }}
+            <h3 class="mt-4 text-lg font-medium text-gray-900">
+              {{ product.name }}
+            </h3>
+            <p class="mt-1 text-sm font-medium text-gray-700">
+              {{ product.price }} TND / day
             </p>
           </a>
         </div>
